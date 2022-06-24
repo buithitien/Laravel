7ee2bc6c4dc524014c222a5794f1bb66dce0e034
buithitien <tien.bui23@stusent.passerellesnumeriques.org>
2022-06-24 11:32:00 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+// phải khai báo, chứa các trường của bảng đó, nếu khai báo thiếu một trường nào đó,
+// khi truy vấn
+// thì nó sẽ báo lỗi không tồn tại
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        // tìm hiểu nhehn
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
+// Objetc relationship mapping modelling mô hình quan hệ đối tượng, 
+//biến các bảng thành các object, những table thành object, table-model
+// eloquent thì khai báo model, tốc độ xử lí chậm hơn so với query builder
+// ngắn gọn dễ hiểu 
